ac35857a228574089e685ed37dbcc02659f56026
HiRahmat-Dev <ahmadalhidayah1@gmail.com>
2020-03-27 16:24:36 +0700

    Add a little bit of error handling

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,6 +1,13 @@
 module.exports = {
   response: (res, result, status, err) => {
     let resultPrint = {};
+    if (status == 404) {
+      resultPrint.status = 'Not Found!';
+      resultPrint.statusCode = status;
+      resultPrint.result = null;
+      resultPrint.err = err || null;
+      return res.status(resultPrint.statusCode).json(resultPrint);
+    }
     resultPrint.status = 'Success!';
     resultPrint.statusCode = status;
     resultPrint.result = result;
