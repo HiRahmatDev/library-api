cbac3035d04e5ab85351087ea2908dd8180977e8
HiRahmat-Dev <ahmadalhidayah1@gmail.com>
2020-04-11 12:53:54 +0700

    Update

--- src/model/book.js
+++ src/model/book.js
@@ -3,6 +3,12 @@ const bookModel = {
   getBooks: (search, sort = 'title', page, limit) => {
     return new Promise((resolve, reject) => {
       if (search) {
+        connection.query('SELECT `books`.*, `category`.`name_category` FROM `books` JOIN `category` ON `books`.`id_category` = `category`.`id` WHERE lower(`books`.`title`) LIKE ? OR lower(`books`.`description`) LIKE ? OR lower(`books`.`author`) LIKE ? OR lower(`category`.`name_category`) LIKE ?', [`%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`], (err, result) => {
+          if(err) {
+            reject(new Error(err));
+          }
+          resolve(result);
+        });
         if (sort) {
           connection.query('SELECT `books`.*, `category`.`name_category` FROM `books` JOIN `category` ON `books`.`id_category` = `category`.`id` WHERE lower(`books`.`title`) LIKE ? OR lower(`books`.`description`) LIKE ? OR lower(`books`.`author`) LIKE ? OR lower(`category`.`name_category`) LIKE ? ORDER BY `books`.`' + sort + '', [`%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`], (err, result) => {
             if(err) {
